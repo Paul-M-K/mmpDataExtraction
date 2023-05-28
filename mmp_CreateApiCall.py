4e7aacce8ea61ce4e3eb3db57344e410e8fe2ce8
PaulK <paul.kot@withyouwithme.com>
2023-05-28 11:34:05 -0700

    save

--- mmp_CreateApiCall.py
+++ mmp_CreateApiCall.py
@@ -11,12 +11,12 @@ for file in os.listdir(folder):
     print(file_split)
     if file_split.find('Pokemon') in file_split:
         print(file_split)
-    # game_name = file_split[0].replace("-", ": ")
-    # game_name = game_name.replace("-", ":")
-    # print(file_split[0])
-    # response = post('https://api.igdb.com/v4/games/',**{'headers': {'Client-ID': 'yizntsh128hmthcof1qurn2eashnvm','Authorization': 'Bearer lkvqnflz33l1bybwvqn1nuotoixt42'},'data': 'fields rating, rating_count, name, platforms; where name = "{}";'.format(game_name)}).text
-    # print("file Name: ", game_name, " response: ",response)
-    # time.sleep(0.25)
+    game_name = file_split[0].replace("-", ": ")
+    game_name = game_name.replace("-", ":")
+    print(file_split[0])
+    response = post('https://api.igdb.com/v4/games/',**{'headers': {'Client-ID': 'yizntsh128hmthcof1qurn2eashnvm','Authorization': 'Bearer lkvqnflz33l1bybwvqn1nuotoixt42'},'data': 'fields rating, rating_count, name, platforms; where name = "{}";'.format(game_name)}).text
+    print("file Name: ", game_name, " response: ",response)
+    time.sleep(0.25)
 
 '''
 response = post('https://api.igdb.com/v4/platforms/', **{'headers': {'Client-ID': 'yizntsh128hmthcof1qurn2eashnvm', 'Authorization': 'Bearer lkvqnflz33l1bybwvqn1nuotoixt42'},'data': 'fields *; where name = "Nintendo Entertainment System"; limit 500;'}).text
