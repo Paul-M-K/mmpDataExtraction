a0540b586c13b3a159d665ad4062f330442298e4
PaulK <paul.kot@withyouwithme.com>
2023-05-28 11:39:00 -0700

    save

--- igdb_APIExtractRatings.py
+++ igdb_APIExtractRatings.py
@@ -10,29 +10,20 @@ import os
 with open('platformsDataextract.json', 'r') as f:
   data = json.load(f)
 
-print(data)
+print(data['platform_details']['id'])
 
 
 for i in data:
     consoleId = str(i['id'])
+    print(consoleId)
     response = post('https://api.igdb.com/v4/platforms/',
                     **{'headers': {'Client-ID': 'yizntsh128hmthcof1qurn2eashnvm',
                                    'Authorization': 'Bearer lkvqnflz33l1bybwvqn1nuotoixt42'},
                        'data': 'fields name, rating, rating_count; sort rating desc; where rating != null & platforms = "{}";'.format(consoleId)})
 
-# fields name,rating, rating_count;
-# sort rating desc;
-# where rating != null & platforms = 24;
-# limit 500;
-
-# print(data['platform_details'][{'id'}])
 
 # import pandas as pd
+
 # df = pd.read_json('platformsDataextract.json')
-# print(df['id'])
 
-import pandas as pd
-
-df = pd.read_json('platformsDataextract.json')
-
-print(df.to_string(['id'])) 
+# print(df.to_string(['id'])) 
