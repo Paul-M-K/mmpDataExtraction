a8d0e2d7319370a3278b3cdccae93a42897b09eb
PaulK <paul.kot@withyouwithme.com>
2023-05-29 10:09:04 -0700

    save

--- igdb_APIExtractRatings.py
+++ igdb_APIExtractRatings.py
@@ -3,7 +3,6 @@ from requests import post
 import time
 import json
 import os
-import matplotlib as plt
 
 # need to use platforms data extraction id's to get a list off all the raitings for all their associated games. 
 
@@ -22,9 +21,3 @@ for i in data:
                                    'Authorization': 'Bearer lkvqnflz33l1bybwvqn1nuotoixt42'},
                        'data': 'fields name, rating, rating_count; sort rating desc; where rating != null & platforms = "{}";'.format(consoleId)})
 
-
-# import pandas as pd
-
-# df = pd.read_json('platformsDataextract.json')
-
-# print(df.to_string(['id'])) 
