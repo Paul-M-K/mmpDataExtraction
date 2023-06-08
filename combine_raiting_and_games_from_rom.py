9d4c7eb75b08e86b00c7c384fe9a0258cce0fb07
PaulK <paul.kot@withyouwithme.com>
2023-06-07 17:29:24 -0700

    big  save

--- combine_raiting_and_games_from_rom.py
+++ combine_raiting_and_games_from_rom.py
@@ -4,6 +4,7 @@ import pandas as pd
 import roman
 import math
 import re
+from spellchecker import SpellChecker
 from openpyxl import Workbook
 from openpyxl.utils.dataframe import dataframe_to_rows
 from fuzzywuzzy import fuzz
@@ -132,6 +133,7 @@ games = games.drop_duplicates()
 ratings = ratings.drop_duplicates()
 
 # remove any game type name like .gba, .zip... etc.
+games = games[~games['Game Name'].str.endswith('.jpg')]
 games['Game Name'] = games['Game Name'].apply(lambda x: x.rsplit(".", 1)[0])
 games['Game Name'] = games['Game Name'].str.strip()
 
@@ -141,13 +143,40 @@ games['Game Name'] = games['Game Name'].str.replace(r"\s*\([UJ]\)", '', regex=Tr
 # fix Pokemon for incoming ratings data
 ratings['Game Name'] = ratings['Game Name'].str.replace("é", "e")
 
+# remove words like the
+games['Game Name'] = games['Game Name'].str.replace("the", " ")
+ratings['Game Name'] = ratings['Game Name'].str.replace("the", " ")
 
-
-# remove any odd characters.
 # Define the characters to remove
 characters_to_remove = r"!@#$%^&*()-_=+[:;\"{,}].<>/?'"
 games['Game Name'] = games['Game Name'].apply(lambda x: re.sub(f"[{re.escape(characters_to_remove)}]", ' ', x).strip())
 ratings['Game Name'] = ratings['Game Name'].apply(lambda x: re.sub(f"[{re.escape(characters_to_remove)}]", ' ', x).strip())
+
+
+def correct_spelling(input_string):
+    spell_checker = SpellChecker()
+    corrected_string = ""
+
+    # Split the input string into individual words
+    words = input_string.split()
+
+    # Check and correct the spelling of each word
+    for word in words:
+        # Get the corrected version of the word if it is misspelled
+        corrected_word = spell_checker.correction(word)
+
+        # Check if a valid correction is found
+        if corrected_word is not None:
+            # Add the corrected word to the corrected string
+            corrected_string += corrected_word + " "
+        else:
+            # If no valid correction is found, keep the original word
+            corrected_string += word + " "
+
+    # Remove the trailing space
+    corrected_string = corrected_string.strip()
+
+    return corrected_string
 
 def convert_last_roman_to_number(string):
     parts = string.split()
@@ -159,61 +188,85 @@ def convert_last_roman_to_number(string):
         pass
     return ' '.join(parts)
 
-ratings['Game Name'] = ratings['Game Name'].apply(convert_last_roman_to_number)
+def add_number_if_missing(string):
+    # Check if the string ends with a number
+    if not re.search(r'\d$', string):
+        # Append "1" to the string
+        string += "1"
+    return string
 
-games.to_excel('games.csv', index=False)
-ratings.to_excel('ratings.csv', index=False)
+ratings['Game Name'] = ratings['Game Name'].apply(correct_spelling)
+games['Game Name'] = games['Game Name'].apply(correct_spelling)
+ratings['Game Name'] = ratings['Game Name'].apply(convert_last_roman_to_number)
+games['Game Name'] = games['Game Name'].apply(convert_last_roman_to_number)
+games['Game Name'] = games['Game Name'].apply(add_number_if_missing)
+ratings['Game Name'] = ratings['Game Name'].apply(add_number_if_missing)
+
+games.to_csv('games.csv', index=False)
+ratings.to_csv('ratings.csv', index=False)
 
 # Remove three-digit numbers from "Game Name" column
 games['Game Name'] = games['Game Name'].apply(lambda x: x.split(maxsplit=1)[1] if len(x.split(maxsplit=1)[0]) == 3 and x.split(maxsplit=1)[0].isdigit() else x)
 
+# make an exeption for play station because it is only 2 numbers foe their naming convention.
+games['Game Name'] = games.apply(lambda row: row['Game Name'].split(maxsplit=1)[1] if row['Console Name'] == 'PS' and len(row['Game Name'].split(maxsplit=1)[0]) == 2 and row['Game Name'].split(maxsplit=1)[0].isdigit() else row['Game Name'], axis=1)
+
+# remove all spaces 
+games['Game Name'] = games['Game Name'].str.replace(' ', '')
+ratings['Game Name'] = ratings['Game Name'].str.replace(' ', '')
+
+# make all lower case
+games['Game Name'] = games['Game Name'].str.lower()
+ratings['Game Name'] = ratings['Game Name'].str.lower()
+
 # Merge the data frames based on matching values
 merged_df = pd.merge(games, ratings, left_on=['Console Name', 'Console Name'], right_on=['Console Name', 'Console Name'])
+merged_df = merged_df[merged_df['Game Name_x'].str[-1] == merged_df['Game Name_y'].str[-1]]
 
 
+print(merged_df)
 
 merged_df['similarity'] = merged_df.apply(lambda row: fuzz.ratio(row['Game Name_x'], row['Game Name_y']), axis=1)
-merged_df = merged_df[merged_df['similarity'] > 0]
-merged_df.to_excel('merged_df.csv', index=False)
-# Sort the DataFrame by 'similarity' column in descending order
-sorted_df = merged_df.sort_values('similarity', ascending=False)
-sorted_df = sorted_df[sorted_df['similarity'] > 60]
-sorted_df.to_excel('sorted_df.csv', index=False)
-print(sorted_df)
 
-# Group the DataFrame by the unique combinations of 'Game Name_x' and 'Game Name_y'
-grouped_df = sorted_df.groupby(['Game Name_x', 'Game Name_y'])
-# print(grouped_df)
+# Sort the DataFrame by 'Game Name_y' and 'similarity' columns in descending order
+sorted_df = merged_df.sort_values(['similarity'], ascending=[False])
 
-# Initialize an empty DataFrame to store the filtered results
-filtered_df = pd.DataFrame(columns=sorted_df.columns)
+# sorted_df = sorted_df[sorted_df['similarity'] > 99]
 
-# Iterate over the groups and keep the unique occurrences with the highest similarity
-for group_name, group_data in grouped_df:
-    max_similarity = group_data['similarity'].max()
-    best_matches = group_data[group_data['similarity'] == max_similarity]
-    filtered_df = pd.concat([filtered_df, best_matches], ignore_index=True)
+sorted_df.to_csv("sorted_df_original_name.csv", index=False)
 
-# Print the filtered DataFrame
-# print(filtered_df)
-filtered_df.to_excel('filtered_df.csv', index=False)
 
-# Filter the rows where similarity is above 95%
-# similarity_df = sorted_df[merged_df['similarity'] > 60]
+# Keep only the first occurrence of each game's original name
+filtered_df = sorted_df.drop_duplicates(subset='Original Name', keep='first')
 
-# Get the index of the first occurrence of each unique 'game_name_x'
-# max_sim_indices = sorted_df.sort_values('Game Name_x',ascending=False)
-# print(max_sim_indices)
+filtered_df.to_csv('result_df.csv', index=False)
 
-# Filter the DataFrame to keep only the rows with the highest similarity
-# filtered_df = similarity_df.loc[max_sim_indices]
 
-# Print the updated DataFrame
-# print(filtered_df)
-# print(games)
+# merged_df = merged_df[merged_df['similarity'] > 0]
+# merged_df.to_csv('merged_df.csv', index=False)
+# # Sort the DataFrame by 'similarity' column in descending order
+# sorted_df = merged_df.sort_values('similarity', ascending=False)
+# sorted_df = sorted_df[sorted_df['similarity'] > 20]
+# sorted_df.to_csv('sorted_df.csv', index=False)
+# print(sorted_df)
 
-# Print the filtered DataFrame
-# filtered_df.to_excel('test.csv', index=False)
-# print(merged_df)
+# # Group the DataFrame by the unique combinations of 'Game Name_x' and 'Game Name_y'
+# grouped_df = sorted_df.groupby(['Game Name_x', 'Game Name_y'])
+# # print(grouped_df)
+
+# # Initialize an empty DataFrame to store the filtered results
+# filtered_df = pd.DataFrame(columns=sorted_df.columns)
+
+# # Iterate over the groups and keep the unique occurrences with the highest similarity
+# for group_name, group_data in grouped_df:
+#     max_similarity = group_data['similarity'].max()
+#     best_matches = group_data[group_data['similarity'] == max_similarity]
+#     filtered_df = pd.concat([filtered_df, best_matches], ignore_index=True)
+
+# # Print the filtered DataFrame
+# # print(filtered_df)
+# filtered_df.to_csv('filtered_df.csv', index=False)
+
+
 
 
