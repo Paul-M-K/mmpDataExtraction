102781ef9cd045eaa9cb96d9f9616fdfe01d2e36
PaulK <paul.kot@withyouwithme.com>
2023-06-14 15:47:58 -0700

    save

--- MoveFoldersUpOneDirectory.py
+++ MoveFoldersUpOneDirectory.py
@@ -1,31 +1,27 @@
 import os
 import shutil
+import sys
+
+def shift_directory(root, folder):
+    src = os.path.join(root, folder)
+    dst = os.path.dirname(root)
+    shutil.move(src, dst)
+
+
+# First we want to reorder the folders for Onion OS.
+# assuming this file pattern is the same for all Miyoo Mini Plus 64 gb.
+def move_folder(directory):
+    for root, dirs, files in os.walk(directory):
+        for folder in dirs:
+            # Move the folder one directory up
+            if folder == 'CPS1+2' or folder == 'CPS3' or folder == 'GBC':
+                shift_directory(root, folder)
+            
 
 def main():
-    # will need to change this.
-    directory = "C:\\Users\\pauru\\Desktop\\Roms-reduced - Copy"
-    
-    # we will use this definition to move the folders up one directory level.
-    
-    
-    def shift_directory(root, folder):
-        src = os.path.join(root, folder)
-        dst = os.path.dirname(root)
-        shutil.move(src, dst)
-    
-    
-    # First we want to reorder the folders for Onion OS.
-    # assuming this file pattern is the same for all Miyoo Mini Plus 64 gb.
-    def move_folder(directory):
-        for root, dirs, files in os.walk(directory):
-            for folder in dirs:
-                # Move the folder one directory up
-                if folder == 'CPS1+2' or folder == 'CPS3' or folder == 'GBC':
-                    shift_directory(root, folder)
-                
-    
-    
-    move_folder(directory)
+    selected_folder = sys.argv[1]  # Access the selected_folder value from command-line arguments
+    move_folder(selected_folder)
 
 if __name__ == '__main__':
     main()
+
